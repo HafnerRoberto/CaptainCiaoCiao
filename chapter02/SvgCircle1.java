7d5e78629cce8273dbe04ef290e8abdb98ad4b27
Robert Hafner <robert.hafner.32@gmail.com>
2025-01-28 20:54:00 +0100

    add svgcircle

---
+++ chapter02/SvgCircle1.java
@@ -0,0 +1,11 @@
+public class SvgCircle1 {
+        public static void main ( String [] args){
+            printXXX();
+        }
+
+        public static void printXXX(){
+            System.out.print("<svg height= '400' width= '1000'>  n" +
+                    " <circle cx='100' cy='100' r='50'/> /n" +
+                    "</svg>");
+        }
+}
