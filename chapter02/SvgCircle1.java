d86460ce40bf8369117307b46cf05c00f75595f2
Robert Hafner <robert.hafner.32@gmail.com>
2025-01-28 21:56:32 +0100

    new line-crazy layout

--- chapter02/SvgCircle1.java
+++ chapter02/SvgCircle1.java
@@ -4,8 +4,8 @@ public class SvgCircle1 {
         }
 
         public static void printXXX(){
-            System.out.print("<svg height= '400' width= '1000'>  n" +
-                    " <circle cx='100' cy='100' r='50'/> /n" +
+            System.out.print("<svg height= '400' width= '1000'> \n" +
+                    " <circle cx='100' cy='100' r='50'/> \n" +
                     "</svg>");
         }
 }
