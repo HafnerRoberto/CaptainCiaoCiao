df21be1cb552e3530db46c065cc60d5ee230ed0a
Robert Hafner <robert.hafner.32@gmail.com>
2025-02-07 09:57:14 +0100

    Math.random - Javadoc and range

---
+++ chapter02/SvgCircle3.java
@@ -0,0 +1,15 @@
+public class SvgCircle3 {
+        public static void main ( String [] args){
+            int x = 100;
+            int y = 110;
+            double r = 10 + Math.random() * 20;
+            double random = Math.random() * (20 - 10) + 10;
+            printXXX(x, y, random);
+        }
+
+        public static void printXXX(int x, int y, double r) {
+            System.out.printf("<svg height= '100' width= '1000'> \n" +
+                    " <circle cx='%d' cy='%d' r='%f'/> \n" +
+                    "</svg>", x,y,r);
+        }
+}
