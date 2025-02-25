4ec8d669715d87a3d8e3e096ccc6d9efe99c9f8c
Robert Hafner <robert.hafner.32@gmail.com>
2025-02-25 19:53:38 +0100

    completed RepeatingChars

--- chapter05/RepeatingChars.java
+++ chapter05/RepeatingChars.java
@@ -1,9 +1,15 @@
-public class ReapitingChars {
+public class RepeatingChars {
 
     static int isEveryCharacterTwice(String input){
         if (input == null) throw new NullPointerException ();
-
-
+        if(input.length() % 2 != 0){
+            return 0;
+        }
+        for(int i = 0; i < input.length(); i = i + 2){
+            if (! (input.charAt(i) == input.charAt(i+1))){
+                return -(i + 1);
+            }
+        }
         return 1;
     }
 
@@ -11,5 +17,7 @@ public class ReapitingChars {
         System.out.println( isEveryCharacterTwice( "eehhrrwwüürrddiiggeerr$$ccaappttaaiinn" ) );
         System.out.println( isEveryCharacterTwice( "ccapptttaaiinn" ) );
         System.out.println( isEveryCharacterTwice( "222" ) );
+       // System.out.println( isEveryCharacterTwice( null ) );
+
     }
 }
