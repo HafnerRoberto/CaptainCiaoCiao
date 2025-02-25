8e7b3a8a5a2813beeb209331447dd775d8e6fc4f
HafnerRoberto <robert.hafner.32@gmail.com>
2025-02-25 13:32:24 +0100

    with help of solution - error found -> htmlElement() should strong and not b

--- chapter05/BuildHtml.java
+++ chapter05/BuildHtml.java
@@ -1,31 +1,37 @@
 public class BuildHtml {
 
-    static String strong (String strong){
-        return "<strong>" + strong + "</strong>";
+    static String strong (String body){
+        return htmlElement("strong", body );
     }
 
-    static String emphaszied(String emph){
+    static String emphasized(String body){
         //
-        return "<em>" + emph +"</em>";
+        return htmlElement("em", body);
     }
 
     static String htmlElement(String tag, String body){
         // if tag is null or leer only body
-        if (tag == null && body == null){
-            return "";
-        } else if (tag == null || tag.isEmpty()) {
+        if (tag == null){
+            tag = "";
+        }
+        if (body == null){
+            body ="";
+        }
+        if (tag.isEmpty()){
             return body;
-        } else {
-            return strong(emphaszied(body));
+        } else{
+            return "<" + tag + ">" + body + "</" + tag + ">";
         }
         // if body == null -> ""
-        
+
     }
 
     public static void main(String[] args) {
-        String tag = "b";
-        String body = "text";
-        String result = htmlElement(tag, body);
-        System.out.println(result);
+        System.out.println( htmlElement( "strong", "strong is bold" ) );
+        System.out.println( strong( emphasized( "strong + emphasized" ) ) ); //only for strong and emphasized
+        System.out.println( htmlElement( "span", null ) );
+        System.out.println( htmlElement( "", "no" ) );
+        System.out.println( htmlElement( null, "not bold" ) );
+        System.out.println( htmlElement( null, null ) );
     }
 }
