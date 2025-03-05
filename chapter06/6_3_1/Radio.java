de2ca2d7cc8bd9b36dbd8b420e0cd3172548d022
HafnerRoberto <robert.hafner.32@gmail.com>
2025-03-05 18:44:21 +0100

    add enum Modulation

---
+++ chapter06/6_3_1/Radio.java
@@ -0,0 +1,68 @@
+public class Radio {
+    private static final int UPPERLIMIT = 100;
+    private static final int DOWNERLIMIT = 0;
+    private boolean isOn;
+    private final int volume = 1;
+    private double frequency;
+    private Modulation modulation = Modulation.AM;
+    public enum Modulation{
+        AM,FM
+    }
+
+    Radio(){
+
+    }
+
+    void setModulation(Modulation modulation){
+        this.modulation = modulation;
+    }
+
+    Modulation getModulation(){
+        return modulation;
+    }
+
+    public double getFrequency() {
+        return frequency;
+    }
+
+    public static double stationNameToFrequency (String string){
+        switch (string){
+            case "Walking the plank":
+                return 98.3;
+            case null:
+                return 0.0;
+            default:
+                return 0.0;
+        }
+    }
+
+    public void setFrequency(double frequency) {
+        this.frequency = frequency;
+    }
+    public int getVolume() {
+        return volume;
+    }
+
+    public void on(){
+        this.isOn = true;
+        System.out.println("on");
+    }
+
+    public void off(){
+        this.isOn = false;
+        System.out.println("off");
+    }
+
+    public boolean isOn(){
+        return this.isOn;
+    }
+
+    @Override
+    public String toString(){
+        return "Radio[volume="+this.volume + " is " + (isOn ? "on":"off") + this.modulation +"]";
+    }
+
+}
+
+
+
