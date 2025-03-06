6fc3e8216ffb7554e7e282fd664103a5fd1c2d07
HafnerRoberto <robert.hafner.32@gmail.com>
2025-03-06 09:47:53 +0100

    add Association assignment - initialize Class in declaration, reference in the other class

--- chapter06/6_5_1/MonitorTube.java
+++ chapter06/6_5_1/MonitorTube.java
@@ -1,4 +1,10 @@
 public class MonitorTube {
+    // only assigned once
+    private final TV tv;
+    // cannot be repaired
+    public MonitorTube(TV tv){
+        this.tv = tv;
+    }
     void on(){
         System.out.println("Monitortube is on");
     }
--- chapter06/6_5_1/TV.java
+++ chapter06/6_5_1/TV.java
@@ -1,16 +1,18 @@
 public class TV {
-    private MonitorTube monitorTube = new MonitorTube();
-
+    private MonitorTube monitorTube = new MonitorTube(this);
+    private boolean isOn;
     public TV(){
 
     }
     void on(){
         System.out.println("TV is on");
+        isOn = true;
         monitorTube.on();
     }
 
     void off(){
         System.out.println("TV is off");
+        isOn = false;
         monitorTube.off();
     }
 }
